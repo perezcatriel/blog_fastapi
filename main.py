bb06efd16a47ea6bb84f9145b45bf2f850985a5e
perezcatriel <perezcatriel@gmail.com>
2023-10-17 13:08:35 -0300

    [FIX] CORS test

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ origins = [
 # Configuración de CORS
 app.add_middleware(
         CORSMiddleware,
-        allow_origins="*",
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
