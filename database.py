cfa0c4edff8885a372f88ec22742945a8b397ddd
perezcatriel <perezcatriel@gmail.com>
2023-10-17 09:39:12 -0300

    postgres

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.future import create_engine
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./test.db"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 Base = declarative_base()
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-
+engine = create_engine(DATABASE_URL, echo=True, future=True)
+SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
 
 def init_db():
     Base.metadata.create_all(bind=engine)
